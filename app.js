346d81bad7f918d4c5ae420be9466646536ddd4a
dan nguyen <nguyendq92@gmail.com>
2022-05-11 15:36:12 -0700

    added helmet configs

--- app.js
+++ app.js
@@ -11,8 +11,8 @@ var indexRouter = require('./routes/index');
 var productsRouter = require('./routes/products');
 var categoriesRouter = require('./routes/categories');
 const ejsMate = require('ejs-mate');
-// var compression = require('compression');
-// var helmet = require('helmet');
+var compression = require('compression');
+var helmet = require('helmet');
 var app = express();
 
 // view engine setup
@@ -27,8 +27,50 @@ mongoose.connect(mongoDB, {useNewUrlParser: true, useUnifiedTopology: true});
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
-// app.use(compression()); //Compress all routes
-// app.use(helmet());
+app.use(compression()); //Compress all routes
+const scriptSrcUrls = [
+  "https://stackpath.bootstrapcdn.com/",
+  "https://kit.fontawesome.com/",
+  "https://cdnjs.cloudflare.com/",
+  "https://cdn.jsdelivr.net/",
+  "https://res.cloudinary.com/dpofkfwhq/"
+];
+const styleSrcUrls = [
+  "https://kit-free.fontawesome.com/",
+  "https://stackpath.bootstrapcdn.com/",
+  "https://fonts.googleapis.com/",
+  "https://use.fontawesome.com/",
+  "https://cdn.jsdelivr.net/",
+  "https://res.cloudinary.com/dpofkfwhq/"
+];
+const connectSrcUrls = [
+  "https://res.cloudinary.com/dpofkfwhq/"
+];
+const fontSrcUrls = ["https://res.cloudinary.com/dpofkfwhq/"];
+app.use(
+  helmet.contentSecurityPolicy({
+      directives: {
+          defaultSrc: [],
+          connectSrc: ["'self'", ...connectSrcUrls],
+          scriptSrc: ["'unsafe-inline'", "'self'", ...scriptSrcUrls],
+          styleSrc: ["'self'", "'unsafe-inline'", ...styleSrcUrls],
+          workerSrc: ["'self'", "blob:"],
+          objectSrc: [],
+          imgSrc: [
+              "'self'",
+              "blob:",
+              "data:",
+              "https://res.cloudinary.com/dpofkfwhq/",
+              "https://images.unsplash.com/"
+          ],
+          fontSrc: ["'self'", ...fontSrcUrls],
+          mediaSrc: ["https://res.cloudinary.com/dpofkfwhq/"],
+          childSrc: ["blob:"]
+      },
+
+      crossOriginEmbedderPolicy: false
+  })
+);
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
